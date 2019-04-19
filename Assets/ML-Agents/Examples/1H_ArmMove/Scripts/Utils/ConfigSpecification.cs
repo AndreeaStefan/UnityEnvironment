8d3ab0969aa69f14852189bf4531504868e3d783
Hubert Zając <hubert.zajac@outlook.com>
2019-04-19 12:32:17 +0200

    tem, remove of game

--- Assets/ML-Agents/Examples/1H_ArmMove/Scripts/Utils/ConfigSpecification.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Assets.ML_Agents.Examples._1ArmMove.Scripts
-{
-    public class ConfigSpecification
-    {
-        public int random_parameter;
-        public float arm_scale;
-    }
-}
