57de362de954e2729c266b50c3c8582f9b184728
Hubert Zając <hubert.zajac@outlook.com>
2019-04-19 12:25:45 +0200

    renamed game, removed examples

---
+++ Assets/ML-Agents/Examples/1H_ArmMove/Scripts/Utils/ConfigSpecification.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.ML_Agents.Examples._1ArmMove.Scripts
+{
+    public class ConfigSpecification
+    {
+        public int random_parameter;
+        public float arm_scale;
+    }
+}
